9d6940275c7439dbe725139194b73c2618313b1e
杨宏超 <1925119849@qq.com>
2024-07-04 10:49:26 +0800

    feat: 导出excel

--- docs/.vitepress/theme/head.ts
+++ docs/.vitepress/theme/head.ts
@@ -1,59 +1,54 @@
-import type { HeadConfig } from 'vitepress';
+import type { HeadConfig } from 'vitepress'
 export const head: HeadConfig[] = [
-  ['link', { rel: 'icon', href: '/favicon.ico' }],
-  ['link', { rel: 'icon', href: '/logo.png' }],
-  ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/logo.png' }],
-  [
-    "script",
-    {
-      crossorigin: "anonymous",
-      async: "",
-      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1279518235550313",
-    },
-  ],
-  [
-    'script',
-    { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-QRQ5599P9G' }
-  ],
-  [
-    'script',
-    {},
-    `window.dataLayer = window.dataLayer || [];
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/logo.png' }],
+    [
+        'script',
+        {
+            crossorigin: 'anonymous',
+            async: '',
+            src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1279518235550313'
+        }
+    ],
+    ['script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-QRQ5599P9G' }],
+    [
+        'script',
+        {},
+        `window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-QRQ5599P9G');`
-  ],
-  [
-    "script",
-    {
-      nonce: "X3-MeXfNjwsxlQdQcRqhtQ",
-      async: "",
-      src: "https://fundingchoicesmessages.google.com/i/pub-1279518235550313?ers=1",
-    },
-  ],
-  [
-    'script',
-    {},
-    `(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`
-  ],
-  [
-    "link",
-    { rel: "stylesheet", href: "/static/css/fancybox.css" },
-  ],
-  ["script", { src: "/static/js/fancybox.umd.js" }],
-  [
-    "link",
-    { rel: "stylesheet", href: "/static/css/carousel.css" },
-  ],
-  ["script", { src: "/static/js/carousel.umd.js" }],
-  [
-    "link",
-    { rel: "stylesheet", href: "/static/css/carousel.thumbs.css" },
-  ],
-  ["script", { src: "/static/js/carousel.thumbs.esm.js" }],
-  [
-    'script',
-    {},
-    `const container = document.getElementById("myCarousel");const options = { infinite: false };new Carousel(container, options);`
-  ]
-];
+    ],
+    [
+        'script',
+        {
+            nonce: 'X3-MeXfNjwsxlQdQcRqhtQ',
+            async: '',
+            src: 'https://fundingchoicesmessages.google.com/i/pub-1279518235550313?ers=1'
+        }
+    ],
+    [
+        'script',
+        {},
+        `(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`
+    ],
+    ['link', { rel: 'stylesheet', href: '/static/css/fancybox.css' }],
+    ['script', { src: '/static/js/fancybox.umd.js' }],
+    ['link', { rel: 'stylesheet', href: '/static/css/carousel.css' }],
+    ['script', { src: '/static/js/carousel.umd.js' }],
+    ['link', { rel: 'stylesheet', href: '/static/css/carousel.thumbs.css' }],
+    ['script', { src: '/static/js/carousel.thumbs.esm.js' }],
+    [
+        'script',
+        {},
+        `const container = document.getElementById("myCarousel");const options = { infinite: false };new Carousel(container, options);`
+    ],
+    // https://api.vvhan.com/api/script/snow  落雪
+    // https://api.vvhan.com/api/script/meihua 梅花
+    // https://api.vvhan.com/api/script/denglong  红灯笼
+    // https://api.vvhan.com/api/script/yinghua  落樱
+    // https://api.vvhan.com/api/script/bolang 冲浪
+
+    ['script', { src: 'https://api.vvhan.com/api/script/yinghua' }]
+]
